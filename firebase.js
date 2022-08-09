ae7097a3c8d8e4fd95d817208595a52e18ef15da
rahsunk <rtkf0522@gmail.com>
2022-08-09 14:30:22 -0400

    Finished code along

--- firebase.js
+++ firebase.js
@@ -1,14 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
 import {
-  getDatabase,
-  ref,
-  push,
-  set,
-  onValue,
+  getDatabase, //to get our database from firebase
+  ref, //gives us specific references in our databases
+  push, //creates a new location in a given reference
+  set, //adds data to our database
+  onValue, //reads data from our database
 } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
 
 // Your web app's Firebase configuration
 // ENTER FIREBASE CONFIG HERE
+const firebaseConfig = {
+  apiKey: "AIzaSyDtyRzkc9Xrnl2z7I4T4E13V4yOrvo2z_c",
+  authDomain: "fir-demo-45e13.firebaseapp.com",
+  projectId: "fir-demo-45e13",
+  storageBucket: "fir-demo-45e13.appspot.com",
+  messagingSenderId: "79112646637",
+  appId: "1:79112646637:web:2c5484927dbe2d371101e4"
+};
 
 
 // Initialize Firebase
--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 import { ref, push, set, database, onValue } from "./firebase.js";
 
 //get elements from index.html
+let userInput=document.getElementById('itemInput')
+let submitBtn=document.getElementById('submitBtn')
+let itemsContainer=document.getElementById('itemsContainer')
 
 //refrence a part of our database
+let itemsRef=ref(database,"items")
 
 //add item to database
+submitBtn.onclick=function(event){
+    event.preventDefault()
+    let userInputVal=userInput.value
+    console.log(userInputVal)
+    userInput.value=""
+
+    let newItemRef=push(itemsRef)
+    set(newItemRef,userInputVal)
+}
 
 //show items on firebase in our items container
+onValue(itemsRef,(snapshot)=>{
+    itemsContainer.innerHTML=""
+    let data=snapshot.val()
+    console.log(data)
+    for(let key in data){
+        console.log(data[key])
+
+        let newLi=document.createElement('li')
+        newLi.innerHTML=data[key]
+        itemsContainer.append(newLi)
+    }
+})
